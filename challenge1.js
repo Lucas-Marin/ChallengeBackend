417b7fc90b1efc3887041e375e5ea1a6b9c00ec9
Lucas <lucasmamarin95@gmail.com>
2024-04-10 10:22:59 -0300

    challenge 2

--- challenge1.js
+++ challenge1.js
@@ -1,43 +1,88 @@
 class ProjectManajer {
-    constructor() {
-      this.productos = [];
-      this.contadorId = 1; // Inicializar el contador de ID en 1
-    }
-  
-    addProduct(title,decription, price, thumbnail, stock) {
-      const nuevoProducto = {
-        code: this.contadorId,
-        title,
-        decription,
-        price,
-        thumbnail,
-        stock,
-      };
-      this.productos.push(nuevoProducto);
-      this.contadorId++; // Incrementar el contador de ID para el siguiente producto
-    }
-    getProductById(code) {
-        return this.productos.find(producto => producto.code === code);
-      }
-  
-    getProducts() {
-      this.productos.forEach(producto => {
-        console.log(`code: ${producto.code}, Nombre: ${producto.title}, decription: ${producto.decription}, price: ${producto.price}, thumbnail: ${producto.thumbnail}, stock: ${producto.stock}`);
-      });
+  constructor() {
+    this.products = [];
+    this.contadorId = 1; // Inicializar el contador de ID en 1
+  }
+
+  addProduct(title, decription, price, thumbnail, stock) {
+    const newProduct = {
+      code: this.contadorId,
+      title,
+      decription,
+      price,
+      thumbnail,
+      stock,
+    };
+    this.products.push(newProduct);
+    this.contadorId++; // Incrementar el contador de ID para el siguiente producto
+  }
+
+  getProductById(code) {
+    return this.products.find((product) => product.code === code);
+  }
+
+  getProducts() {
+    this.products.forEach((product) => {
+      console.log(
+        `code: ${product.code}, Nombre: ${product.title}, decription: ${product.decription}, price: ${product.price}, thumbnail: ${product.thumbnail}, stock: ${product.stock}`
+      );
+    });
+  }
+
+  deleteProduct(code) {
+    this.products = this.products.filter((product) => product.code !== code);
+  }
+
+  updateProduct(code, newTitle, newPrice, newDecription, newThumbnail, newStock) {
+    const product = this.products.find(product => product.code === code);
+    if (product) {
+      product.title = newTitle !== undefined ? newTitle : product.Title;
+      product.price = newPrice !== undefined ? newPrice : product.price;
+      product.decription = newThumbnail !== undefined ? newThumbnail : product.thumbnail;
+      product.decription = newStock !== undefined ? newStock : product.stock;
+      product.decription = newDecription !== undefined ? newDecription : product.decription;
+      console.log(`Producto ${code} actualizado`);
+    } else {
+      console.log(`No se encontró ningún producto ${code}`);
     }
   }
-  const grupoProductos = new ProjectManajer();
+}
+const groupOfProducts = new ProjectManajer();
 
-grupoProductos.addProduct('manzana', 'rojo', 10, 'https://imagen.com/manzana', 9);
-grupoProductos.addProduct('naranja', 'naranja',20, 'https://imagen.com/naranja', 15);
-grupoProductos.addProduct('limon', 'amarillo', 30, 'https://imagen.com/limon',16);
+groupOfProducts.addProduct(
+  "manzana",
+  "rojo",
+  10,
+  "https://imagen.com/manzana",
+  9
+);
+groupOfProducts.addProduct(
+  "naranja",
+  "naranja",
+  20,
+  "https://imagen.com/naranja",
+  15
+);
+groupOfProducts.addProduct(
+  "limon",
+  "amarillo",
+  30,
+  "https://imagen.com/limon",
+  16
+);
 
-grupoProductos.getProducts();
+groupOfProducts.getProducts();
 
-const producto = grupoProductos.getProductById(4);
+const product = groupOfProducts.getProductById(5);
 
-if (producto) {
-  console.log(`code: ${producto.code}, Nombre: ${producto.title}, decription: ${producto.decription}, price: ${producto.price}, thumbnail: ${producto.thumbnail}, stock: ${producto.stock} `);
+if (product) {
+  console.log(
+    `code: ${product.code}, Name: ${product.title}, Decription: ${product.decription}, Price: ${product.price}, Thumbnail: ${product.thumbnail}, Stock: ${product.stock} `
+  );
 } else {
   console.log("Not Found");
 }
+
+groupOfProducts.updateProduct(1,'calabaza',14 ,'grande y anaranjado','https://imagen.com/calabaza',3 );
+
+groupOfProducts.getProducts();
